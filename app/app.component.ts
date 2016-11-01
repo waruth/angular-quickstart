c66d7992cf7a9c55afaddae4d1f56af10ade7e40
waruth <hwaruth@gmail.com>
2016-11-01 18:03:02 +0700

    angular-quickstart

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+/** step 2 Create your application */
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: '<h1>Hello World</h1>\
+    <p>My first angular app!</p>'
+})
+export class AppComponent { }
---
+++ app/app.module.ts
@@ -0,0 +1,13 @@
+/** step 2 Create your application */
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+/** step 3 Create a component and add it to your application */
+import { AppComponent }   from './app.component';
+
+@NgModule({
+  imports:      [ BrowserModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
